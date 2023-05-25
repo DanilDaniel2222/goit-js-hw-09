5e800733b1d1720f6f7c0a3617e8dc21775c196f
Danil Kuskalo <danilkuskalo1999@gmail.com>
2023-05-25 14:50:43 +0300

    huge commit

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,3 +1,35 @@
+const start = document.querySelector('[data-start]');
+const stop = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
   }
+
+let timerId = null;
+
+start.addEventListener("click", () => {
+    console.log('start');
+    timerId = setInterval(() => {
+        body.style.background = getRandomHexColor();
+        start.disabled = true;
+    }, 1000);
+});
+
+stop.addEventListener("click", () => {
+    console.log('stop');
+    clearInterval(timerId);
+    start.disabled = false;
+});
+
+
+
+
+
+
+
+
+
+
+
+
